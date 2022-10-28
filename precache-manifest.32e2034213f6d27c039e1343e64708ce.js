0d37ec80577c27edbaf5e9d8c131bddeeec426e7
Dan Chui <dchui108@gmail.com>
2022-10-28 09:27:11 +0900

    Updates

---
+++ precache-manifest.32e2034213f6d27c039e1343e64708ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "73812801b9e63b7ff7dd",
+    "url": "/robo-friends/static/css/main.3a7929f3.chunk.css"
+  },
+  {
+    "revision": "73812801b9e63b7ff7dd",
+    "url": "/robo-friends/static/js/main.73812801.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/robo-friends/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "978e0d8eac9382ae649c",
+    "url": "/robo-friends/static/css/2.0a094587.chunk.css"
+  },
+  {
+    "revision": "978e0d8eac9382ae649c",
+    "url": "/robo-friends/static/js/2.978e0d8e.chunk.js"
+  },
+  {
+    "revision": "64431f98b918f2dfebbc",
+    "url": "/robo-friends/static/js/runtime~main.64431f98.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ce3f21f97da3c5672c02571775a31699",
+    "url": "/robo-friends/index.html"
+  }
+];
